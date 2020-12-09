5883367afac49dd280afc7f1bc01983e33e70062
twzkraus <twzkraus@gmail.com>
2020-12-09 11:06:41 -0800

    implements part 2 of day9 advent of code

--- day9.js
+++ day9.js
@@ -1098,4 +1098,73 @@ console.log('first violation in example is', solvePart1(p1ex, 5));
 console.log('first violation in puzzle is', solvePart1(puzzle, 25));
 // 90433990 is correct
 
+/*
+--- Part Two ---
+The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
 
+Again consider the above example:
+
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+
+To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
+
+What is the encryption weakness in your XMAS-encrypted list of numbers?
+*/
+
+// find contiguous set
+
+const solvePart2 = (puzzle, preambleLength) => {
+  const nums = puzzle.split('\n');
+  nums.forEach((num, i) => {
+    nums[i] = parseInt(num);
+  });
+
+  const target = solvePart1(puzzle, preambleLength);
+
+  return getEncryptionWeakness(nums, target);
+};
+
+const getEncryptionWeakness = (numbers, targetNum) => {
+  let runningTotal = 0;
+  let start = 0;
+  let end = 1;
+  while (start < numbers.length) {
+    runningTotal += numbers[start];
+    while (runningTotal < targetNum) {
+      runningTotal += numbers[end];
+      end++;
+    }
+    if (runningTotal === targetNum) {
+      const run = numbers.slice(start, end - 1);
+      return Math.min(...run) + Math.max(...run);
+    } else {
+      runningTotal = 0;
+      start++;
+      end = start + 1;
+    }
+  }
+};
+
+console.log('encryption weakness for example is', solvePart2(p1ex, 5));
+console.log('encryption weakness for puzzle is', solvePart2(puzzle, 25));
+// 11691646 is correct
