087042954d05c82ad006824b886f3b808238a8f3
Turner Kraus <twzkraus@gmail.com>
2021-12-03 12:04:15 -0800

    solved: part2 day1

--- 2021/day1.ts
+++ 2021/day1.ts
@@ -198,7 +198,7 @@ const puzzleInput = [
   8511, 8530, 8531, 8537, 8536, 8537, 8544, 8549, 8562, 8563, 8562,
 ];
 
-const numLarger = puzzleInput.reduce(
+const part1 = puzzleInput.reduce(
   (result, depth) => {
     if (result.previous !== null && depth > result.previous) {
       result.numLarger++;
@@ -208,4 +208,48 @@ const numLarger = puzzleInput.reduce(
   },
   { previous: null, numLarger: 0 }
 );
-console.log("answer is", numLarger.numLarger);
+console.log("part 1 answer is", part1.numLarger);
+
+/* 
+--- Part Two ---
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A      
+200  A B    
+208  A B C  
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G  
+269    F G H
+260      G H
+263        H
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+*/
+
+let part2SumsLarger = 0;
+
+for (let i = 3; i < puzzleInput.length; i++) {
+  const currentNum = puzzleInput[i];
+  const threeBack = puzzleInput[i - 3];
+  currentNum > threeBack && part2SumsLarger++;
+}
+console.log("part 2 answer is", part2SumsLarger);
