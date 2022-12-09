cd1da70db5d776308c4428a777353a0977a64c1a
Turner Kraus <twzkraus@gmail.com>
2022-12-09 15:48:05 -0800

    solved: day2 part2

--- 2022/day2.js
+++ 2022/day2.js
@@ -2606,3 +2606,64 @@ const solvePart1 = (puzzleInput) => {
 
 console.log("part1 example result", solvePart1(exampleInput1));
 console.log("part1 result", solvePart1(puzzleInput1));
+
+/*
+--- Part Two ---
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+*/
+// i: tuples for each round, including opponent's play and desired result
+// c: score is calculated by score of shape (rock: 1, paper: 2, scissors: 3), plus score of outcome (loss: 0, draw: 3, win: 6)
+// o: total score if following the strategy guide
+
+// need to work backwards from opponent hand and result to get myhand
+
+const resultMap = {
+  X: LOSE,
+  Y: DRAW,
+  Z: WIN,
+};
+
+const getMyHand = (opponentsHand, resultCode) => {
+  if (resultMap[resultCode] === DRAW) {
+    return rpsMap[opponentsHand];
+  }
+  // opponent throws rock: win with paper, lose with scissors
+  if (rpsMap[opponentsHand] === ROCK) {
+    if (resultMap[resultCode] === WIN) return PAPER;
+    return SCISSORS;
+  }
+  // opponent throws paper: win with scissors, lose with rock
+  if (rpsMap[opponentsHand] === PAPER) {
+    if (resultMap[resultCode] === WIN) return SCISSORS;
+    return ROCK;
+  }
+  // opponent throws scissors: win with rock, lose with paper
+  if (rpsMap[opponentsHand] === SCISSORS) {
+    if (resultMap[resultCode] === WIN) return ROCK;
+    return PAPER;
+  }
+};
+
+const solvePart2 = (puzzleInput) => {
+  const rounds = puzzleInput.split("\n");
+  const totalScore = rounds.reduce((total, round) => {
+    const [opponentsHandCode, resultCode] = round.split(" ");
+    const myHand = getMyHand(opponentsHandCode, resultCode);
+    const result = resultMap[resultCode];
+    const roundScore = scoreRound(result, myHand);
+    return total + roundScore;
+  }, 0);
+  return totalScore;
+};
+
+console.log("part2 example result", solvePart2(exampleInput1));
+console.log("part2 result", solvePart2(puzzleInput1));
