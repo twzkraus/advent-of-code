a4fbe4667ad92fa3d609f3092c84dfbca09ad2dc
Turner Kraus <twzkraus@gmail.com>
2022-12-16 16:23:40 -0800

    solved: day6 part2

--- 2022/day6.js
+++ 2022/day6.js
@@ -31,7 +31,7 @@ How many characters need to be processed before the first start-of-packet marker
 */
 
 const { readFileSync } = require("fs");
-
+const exampleInput0 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
 const exampleInput1 = "bvwbjplbgvbhsrlpgdmjqwftvncz";
 const exampleInput2 = "nppdvjthqldpwncqszvftbrmjlhg";
 const exampleInput3 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
@@ -63,8 +63,34 @@ const solvePart1 = (puzzleInput) => {
   return findFirstSetOfNUniqueChars(puzzleInput, 4);
 };
 
+console.log(solvePart1(exampleInput0), "should be 7");
 console.log(solvePart1(exampleInput1), "should be 5");
 console.log(solvePart1(exampleInput2), "should be 6");
 console.log(solvePart1(exampleInput3), "should be 10");
 console.log(solvePart1(exampleInput4), "should be 11");
 console.log("part 1 answer is", solvePart1(puzzleInput1));
+
+/*
+--- Part Two ---
+Your device's communication system is correctly detecting packets, but still isn't working. It looks like it also needs to look for messages.
+
+A start-of-message marker is just like a start-of-packet marker, except it consists of 14 distinct characters rather than 4.
+
+Here are the first positions of start-of-message markers for all of the above examples:
+
+mjqjpqmgbljsphdztnvjfqwrcgsmlb: first marker after character 19
+bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 23
+nppdvjthqldpwncqszvftbrmjlhg: first marker after character 23
+nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 29
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 26
+How many characters need to be processed before the first start-of-message marker is detected?
+*/
+
+const solvePart2 = (puzzleInput) => findFirstSetOfNUniqueChars(puzzleInput, 14);
+
+console.log(solvePart2(exampleInput0), "should be 19");
+console.log(solvePart2(exampleInput1), "should be 23");
+console.log(solvePart2(exampleInput2), "should be 23");
+console.log(solvePart2(exampleInput3), "should be 29");
+console.log(solvePart2(exampleInput4), "should be 26");
+console.log("part 2 answer is", solvePart2(puzzleInput1));
